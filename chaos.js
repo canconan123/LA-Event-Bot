8660dccfabbd4ebfb2496dbee4019d86d79328d2
soulden1202 <74317980+soulden1202@users.noreply.github.com>
2022-02-28 03:12:29 -0500

    LA v1

---
+++ chaos.js
@@ -0,0 +1,145 @@
+const data = {
+  chaos: [
+    {
+      name: "Chaos Gate",
+      day: "Monday",
+      time: [
+        "1:00",
+        "2:00",
+        "3:00",
+        "4:00",
+        "5:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        "23:00",
+        "23:59",
+      ],
+      location: "Depends on gear score",
+      message: "Chaos Gate",
+    },
+    {
+      name: "Chaos Gate",
+      day: "Tuesday",
+      time: ["1:00", "2:00", "3:00", "4:00", "5:00"],
+      location: "Depends on gear score",
+      message: "Chaos Gate",
+    },
+    {
+      name: "Chaos Gate",
+      day: "Thursday",
+      time: [
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        "23:00",
+        "23:59",
+      ],
+      location: "Depends on gear score",
+      message: "Chaos Gate",
+    },
+    {
+      name: "Chaos Gate",
+      day: "Friday",
+      time: ["1:00", "2:00", "3:00", "4:00", "5:00"],
+      location: "Depends on gear score",
+      message: "Chaos Gate",
+    },
+    {
+      name: "Chaos Gate",
+      day: "Saturday",
+      time: [
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        "23:00",
+        "23:59",
+      ],
+      location: "Depends on gear score",
+      message: "Chaos Gate",
+    },
+    {
+      name: "Chaos Gate",
+      day: "Sunday",
+      time: [
+        "1:00",
+        "2:00",
+        "3:00",
+        "4:00",
+        "5:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        "23:00",
+        "24:00",
+      ],
+      location: "Depends on gear score",
+      message: "Chaos Gate",
+    },
+  ],
+};
+const chaosf = (time, day) => {
+  var remaintime;
+  data.chaos.forEach((ctime) => {
+    if (day == ctime.day) {
+      ctime.time.forEach((etime) => {
+        let timec = time.split(":");
+        let etimec = etime.split(":");
+
+        if (timec[0] == etimec[0] - 1 && 60 - 20 <= timec[1]) {
+          console.log(time[1]);
+          remaintime = 60 - timec[1];
+        } else {
+          remaintime = null;
+        }
+      });
+    }
+  });
+
+  return remaintime;
+};
+
+const getEventName = () => {
+  return data.chaos[0].name;
+};
+
+module.exports = {
+  chaosf: chaosf,
+  getEventName: getEventName,
+};
---
+++ main.js
@@ -0,0 +1,110 @@
+const { Client, Intents } = require("discord.js");
+
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES);
+
+//import functions
+let chaosf = require("./chaos");
+
+//idobejct;
+var idobject = {};
+const client = new Client({
+  intents: myIntents,
+});
+
+const prefix = "~";
+let channelid = "947415388235399179";
+let guildid = "811432705446510612";
+
+client.once("ready", () => {
+  console.log("Bot online");
+});
+
+function fn10minutes() {
+  const guild = client.guilds.cache.get(guildid);
+
+  var currentDate = new Date();
+  var psttime = currentDate.toLocaleString("en-US", {
+    hourCycle: "h24",
+    timeZone: "America/Los_Angeles",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  var pstday = currentDate.toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles",
+    weekday: "long",
+  });
+
+  //chaos timer
+  var timechaos = chaosf.chaosf(psttime, pstday);
+
+  let chaosid = idobject[chaosf.getEventName()];
+
+  console.log(timechaos);
+  if (timechaos != null) {
+    guild.channels.cache
+      .get(channelid)
+      .send(
+        "<@&" +
+          chaosid +
+          ">" +
+          "there will be a chaos gate in " +
+          timechaos +
+          " minutes."
+      );
+  }
+}
+
+client.on("messageCreate", (message) => {
+  const guildss = client.guilds.cache.get(guildid);
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command === "createroles" && message.author.id == message.guild.ownerId) {
+    createroles(message);
+  } else if (
+    command === "generateroleid" &&
+    message.author.id == message.guild.ownerId
+  ) {
+    Object.keys(idobject).forEach((k) => delete idobject[k]);
+    message.guild.roles.cache.forEach(
+      (role) => (idobject[role.name] = role.id)
+    );
+
+    guildss.channels.cache.get(channelid).send("Alram Start");
+    setInterval(fn10minutes, 600000);
+  }
+
+  //getroles
+  else if (command === "chaosgate") {
+    if (
+      message.member.roles.cache.some(
+        (role) => role.name == chaosf.getEventName()
+      )
+    ) {
+      guildss.channels.cache.get(channelid).send("Role Alreasdy Exist");
+      return;
+    } else {
+      message.member.roles.add(idobject[chaosf.getEventName()]);
+    }
+  }
+});
+
+const createroles = (message) => {
+  //chaos role
+  if (message.guild.roles.cache.find((role) => role.name == "Chaos Gate")) {
+    console.log("role exist");
+    return false;
+  } else {
+    message.guild.roles
+      .create({
+        name: chaosf.getEventName(),
+        color: "#c90076",
+      })
+      .catch(console.error);
+  }
+};
+
+client.login("OTQ3MzU0NDIzNjc4NjA3NDEw.YhsCjw.JNkAG2rK4YO-9vcyhs_AFD-RWZM");
